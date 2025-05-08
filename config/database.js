eb3e2d4f73ef833d19a10422e718580556f7d566
Miradji-Ladhidhou <ladhidhoum@gmail.com>
2025-05-08 18:51:50 +0400

    sequelize

--- config/database.js
+++ config/database.js
@@ -4,16 +4,17 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 // Configuration de la connexion à la base de données en utilisant les variables d'environnement.
-module.exports = {
-  development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+const sequelize = new Sequelize(
+  process.env.DB_NAME,       
+  process.env.DB_USER,      
+  process.env.DB_PASSWORD,   
+  {
     host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT || 'mysql',
     port: process.env.DB_PORT || 3306,
-    dialect: process.env.DB_DIALECT || 'mysql'
+    logging: false,
   }
-};
+);
 
 // Vérification de la connexion à la base de données.
 sequelize.authenticate()
